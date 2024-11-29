c45e678a2da3a377e18bda44fa18676658fa59db
Jasper Frumau <jasper@imagewize.com>
2024-11-29 18:31:47 +0700

    basic nav component & bud update

--- site/web/app/themes/nynaeve/bud.config.js
+++ site/web/app/themes/nynaeve/bud.config.js
@@ -23,7 +23,7 @@ export default async (app) => {
    *
    * @see {@link https://bud.js.org/reference/bud.setPublicPath}
    */
-  app.setPublicPath('/app/themes/sage/public/');
+  app.setPublicPath('/app/themes/nynaeve/public/');
 
   /**
    * Development server settings
@@ -34,7 +34,7 @@ export default async (app) => {
    */
   app
     .setUrl('http://localhost:3000')
-    .setProxyUrl('http://example.test')
+    .setProxyUrl('http://imagewize.test')
     .watch(['resources/views', 'app']);
 
   /**
