c26ee35927dad40fb5ec53df36298324044161f7
Jasper Frumau <jasper@imagewize.com>
2024-11-30 08:39:49 +0700

    branding moval to navbar

--- site/web/app/themes/nynaeve/resources/views/sections/header.blade.php
+++ site/web/app/themes/nynaeve/resources/views/sections/header.blade.php
@@ -1,8 +1,5 @@
-<header class="banner">
-  <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
-  </a>
-
+<header class="banner bg-neutral-900">
+  {{-- branding moved to resources/views/components/navigation.blade.php --}}
   @if (has_nav_menu('primary_navigation'))
     <x-navigation name="primary_navigation" />
   @endif
