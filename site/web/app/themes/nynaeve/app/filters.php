b753665ac12d3da3c5f7165502e8b85ae62280e0
Jasper Frumau <jasper@imagewize.com>
2024-12-02 18:24:53 +0700

    acf json

--- site/web/app/themes/nynaeve/app/filters.php
+++ site/web/app/themes/nynaeve/app/filters.php
@@ -14,3 +14,20 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+/**
+ * Remove the default widgets editor depenedencies check.
+ *
+ * @return void
+ */
+remove_filter('admin_head', 'wp_check_widget_editor_deps');
+
+/**
+ * Set ACF JSON Save Path Location
+ *
+ * @return string
+ */
+function my_acf_json_save_point($path) {
+    return get_stylesheet_directory() . '/resources/acf-json';
+}
+add_filter('acf/settings/save_json', __NAMESPACE__ . '\\my_acf_json_save_point');
