b928913c04840034347e55a06de9c017cc196c8d
Jasper Frumau <jasper@imagewize.com>
2024-12-28 15:03:56 +0700

    page and post styling improvements

--- site/web/app/themes/nynaeve/resources/views/partials/content-page.blade.php
+++ site/web/app/themes/nynaeve/resources/views/partials/content-page.blade.php
@@ -1,7 +1,9 @@
-@php(the_content())
+<div class="container mx-auto max-w-5xl my-16">
+  @php(the_content())
 
-@if ($pagination)
-  <nav class="page-nav" aria-label="Page">
-    {!! $pagination !!}
-  </nav>
-@endif
+  @if ($pagination)
+    <nav class="page-nav" aria-label="Page">
+      {!! $pagination !!}
+    </nav>
+  @endif
+</div>
--- site/web/app/themes/nynaeve/resources/views/partials/content-single.blade.php
+++ site/web/app/themes/nynaeve/resources/views/partials/content-single.blade.php
@@ -1,23 +1,31 @@
-<article @php(post_class('h-entry container mx-auto'))>
-  <header>
-    <h1 class="p-name font-open-sans font-semibold text-2xl text-black mb-8 antialiased">
-      {!! $title !!}
-    </h1>
+<article @php(post_class('h-entry container mx-auto max-w-5xl'))>
+  <div class="grid grid-cols-1 lg:grid-cols-3 gap-8">
+    <div class="lg:col-span-2">
+      <header>
+        <h1 class="p-name font-open-sans font-semibold text-2xl text-black mb-8 antialiased mt-10">
+          {!! $title !!}
+        </h1>
 
-    @include('partials.entry-meta')
-  </header>
+        @include('partials.entry-meta')
+      </header>
 
-  <div class="e-content">
-    @php(the_content())
+      <div class="e-content mt-3">
+        @php(the_content())
+      </div>
+
+      @if ($pagination)
+        <footer>
+          <nav class="page-nav" aria-label="Page">
+            {!! $pagination !!}
+          </nav>
+        </footer>
+      @endif
+
+      @php(comments_template())
+    </div>
+
+    <div class="lg:col-span-1 mt-10">
+      @include('sections.sidebar')
+    </div>
   </div>
-
-  @if ($pagination)
-    <footer>
-      <nav class="page-nav" aria-label="Page">
-        {!! $pagination !!}
-      </nav>
-    </footer>
-  @endif
-
-  @php(comments_template())
 </article>
