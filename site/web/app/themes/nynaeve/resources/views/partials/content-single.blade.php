87585930edbf4194c8d3ad480a6146da20a8ec55
Jasper Frumau <jasper@imagewize.com>
2024-12-30 10:15:15 +0700

    minor post styling

--- site/web/app/themes/nynaeve/resources/views/partials/content-single.blade.php
+++ site/web/app/themes/nynaeve/resources/views/partials/content-single.blade.php
@@ -1,15 +1,15 @@
-<article @php(post_class('h-entry container mx-auto max-w-5xl'))>
+<article @php(post_class('h-entry container mx-auto max-w-5xl mt-20'))>
   <div class="grid grid-cols-1 lg:grid-cols-3 gap-8">
     <div class="lg:col-span-2">
       <header>
-        <h1 class="p-name font-open-sans font-semibold text-2xl text-black mb-8 antialiased mt-10">
+        <h1 class="p-name font-open-sans font-bold text-3xl text-black mb-8 antialiased">
           {!! $title !!}
         </h1>
 
         @include('partials.entry-meta')
       </header>
 
-      <div class="e-content mt-3">
+      <div class="e-content mt-3 mb-10">
         @php(the_content())
       </div>
 
@@ -24,7 +24,7 @@
       @php(comments_template())
     </div>
 
-    <div class="lg:col-span-1 mt-10">
+    <div class="lg:col-span-1">
       @include('sections.sidebar')
     </div>
   </div>
