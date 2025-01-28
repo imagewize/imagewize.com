27a74e4b9ab07e7343add95b9ff84499cd7a73da
Jasper Frumau <jasper@imagewize.com>
2025-01-28 14:27:24 +0700

    Secondary Pages Builder

--- site/web/app/themes/nynaeve/resources/views/partials/content-page.blade.php
+++ site/web/app/themes/nynaeve/resources/views/partials/content-page.blade.php
@@ -1,3 +1,19 @@
+@if(have_rows('page_builder'))
+    @while(have_rows('page_builder'))
+        @php(the_row())
+
+        @if(get_row_layout() == 'page_intro_block')
+            <section class="py-24 bg-bgGray">
+                <div class="container mx-auto max-w-5xl px-4">
+                    <div class="prose text-textBodyGray text-base leading-loose font-open-sans">
+                        {!! get_sub_field('intro_text') !!}
+                    </div>
+                </div>
+            </section>
+        @endif
+    @endwhile
+@endif
+
 <div class="container mx-auto max-w-5xl my-16 font-open-sans text-textBodyGray text-base leading-loose">
   @php(the_content())
 
