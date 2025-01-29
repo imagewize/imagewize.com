80064c57d5c8bd75b7555d06fe3d99192b5ebe0d
Jasper Frumau <jasper@imagewize.com>
2025-01-29 09:15:23 +0700

    remove page flexible content

--- site/web/app/themes/nynaeve/resources/views/partials/content-page.blade.php
+++ site/web/app/themes/nynaeve/resources/views/partials/content-page.blade.php
@@ -1,19 +1,3 @@
-@if(have_rows('page_builder'))
-    @while(have_rows('page_builder'))
-        @php(the_row())
-
-        @if(get_row_layout() == 'page_intro_block')
-            <section class="py-24 bg-bgGray">
-                <div class="container mx-auto max-w-5xl px-4">
-                    <div class="prose text-textBodyGray text-base leading-loose font-open-sans">
-                        {!! get_sub_field('intro_text') !!}
-                    </div>
-                </div>
-            </section>
-        @endif
-    @endwhile
-@endif
-
 <div class="page-container mb-16 font-open-sans text-textBodyGray text-base leading-loose">
   @php(the_content())
 
